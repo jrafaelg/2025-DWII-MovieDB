dc4d1ef34926493a8071f99c585d4b434e2b4032
Rafael Gonçalves <jrafaelg@live.com>
2025-10-06 17:57:17 -0300

    criando as classes do domínio de dados pessoa

--- moviedb/models/pessoa.py
+++ moviedb/models/pessoa.py
@@ -1,6 +1,7 @@
 import uuid
 
-from sqlalchemy import Column, String, Uuid, Date, Text
+from sqlalchemy import Column, String, Uuid, Date, Text, ForeignKey
+from sqlalchemy.orm import relationship
 
 from moviedb.models.mixins import BasicRepositoryMixin
 from moviedb import db
@@ -10,11 +11,46 @@ class Pessoa(db.Model, BasicRepositoryMixin):
     __tablename__ = 'pessoas'
 
     id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    nome = Column(String(60), nullable=False)
+    nome = Column(String(250), nullable=False)
     nacionalidade = Column(String(60), nullable=False)
     nascimento = Column(Date)
     biografia = Column(Text)
-    poster_principal = Column(Text, nullable=True, default=None)
+    foto_base64 = Column(Text, nullable=True, default=None)
+    avatar_base64 = Column(Text, nullable=True, default=None)
+
+    sexo_id = Column(Uuid(as_uuid=True), ForeignKey('sexos.id'))
+    sexo = relationship("Sexo", back_populates="pessoa")
+
+    ator = relationship(
+        'Ator',
+        back_populates='pessoa',
+        uselist=False,
+        cascade='all, delete-orphan'
+    )
+
+class Ator(db.Model, BasicRepositoryMixin):
+    __tablename__ = 'atores'
+
+    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    nome_artistico = Column(String(250), nullable=False)
+
+    # Chave estrangeira (lado "muitos" do relacionamento)
+    pessoa_id = Column(Uuid(as_uuid=True), ForeignKey('pessoas.id'), unique=True, nullable=False)
+
+    # Relacionamento one-to-one
+    pessoa = relationship('Pessoa', back_populates='ator')
+
+
+class Sexo(db.Model, BasicRepositoryMixin):
+    __tablename__ = 'sexos'
+    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    nome = Column(String(30), nullable=False)
+    pessoa = relationship("Pessoa", back_populates="sexo")
+
+
+
+
+
 
 
 
